073cc8d588c7b8d63b87fc426b78766cc0bb1861
Steven Bunde <stevenbunde2020@gmail.com>
2023-08-06 11:16:22 -0400

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var APIkey = "bccf102cf897ddc4659f3f10d43085ab";
+
+// EX for city: var cityName;
+// URL for searching for city https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// concat the two:
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+//fecth(queryURL)
