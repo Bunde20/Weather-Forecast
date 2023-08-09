0ac87ef54a46de4f5e758f00dcb7a0430e0deeee
Steven Bunde <stevenbunde2020@gmail.com>
2023-08-09 13:11:39 -0400

    setup the city name and current date to append to DOM

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,10 @@
 var APIkey = "bccf102cf897ddc4659f3f10d43085ab";
 var cityInput = document.querySelector(".form-control")
 var searchBtn = document.querySelector(".searchBtn")
+var cityInfo = document.querySelector(".cityInfo")
+var temp = document.querySelector(".temp")
+var wind = document.querySelector(".wind")
+var humidity = document.querySelector(".humidity")
 
 
 searchBtn.addEventListener("click", function() {
@@ -15,5 +19,10 @@ function getCity(city) {
     })
     .then(function(data) {
         console.log(data)
+        var date = new Date()
+        var month = date.getMonth() + 1
+        var day = date.getDate()
+        var year = date.getFullYear()
+        cityInfo.innerHTML = `${data.name} - ${month}/${day}/${year}`
     })
 }
