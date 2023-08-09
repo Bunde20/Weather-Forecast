1932ba4b78b1c342e5d24b48b2c882da4543757f
Steven Bunde <stevenbunde2020@gmail.com>
2023-08-09 13:08:04 -0400

    setup current card for current weather data and functionality to pull current data

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,19 @@
 var APIkey = "bccf102cf897ddc4659f3f10d43085ab";
+var cityInput = document.querySelector(".form-control")
+var searchBtn = document.querySelector(".searchBtn")
 
-// EX for city: var cityName;
-// URL for searching for city https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-// concat the two:
-// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-//fecth(queryURL)
+
+searchBtn.addEventListener("click", function() {
+    var cityName = cityInput.value
+    getCity(cityName)
+})
+
+function getCity(city) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}&units=imperial`)
+    .then(function(res) {
+        return res.json()
+    })
+    .then(function(data) {
+        console.log(data)
+    })
+}
